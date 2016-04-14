b106b981fd5bf286ee77602adeb7da64541a0666
arsenicum32 <zombiehot@yandex.ru>
2016-04-14 10:32:31 +0300

    add full support

--- static/js/about/UI.js
+++ static/js/about/UI.js
@@ -1,6 +1,10 @@
 (function() {
   var hamburger = svg.append('g');
 
+  window.tooltip = d3.select("body")
+    .append("div").style('z-index', 1000).attr('class', 'tooltip')
+    .text("a simple tooltip");
+
   var crs = hamburger.append('circle').attr({
     "class": "hamburger",
     "cx":  60,
@@ -27,7 +31,17 @@
       });
     }
   });
-  hamburger.on('click', function(){
+  hamburger.on({
+    'click':function(){
     effects.go('/nodoor');
+    },
+    'mouseover': function(){
+      tooltip.style("visibility", "visible");
+      tooltip.text( 'на главную' );
+      tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");  /// чтобы понятно было
+    },
+    'mouseout': function(){
+      tooltip.style("visibility", "hidden");  /// чтобы понятно было
+    }
   });
 })();
